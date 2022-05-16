4f1f19f4cb27df907582ac8bc4bee443f096354e
Matt Schell <mschell@doitt.nyc.gov>
2022-05-16 16:07:36 -0400

    corrects paramameter in setup script

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
 CALL sqlcmd -i src\main\teardown.sql
-CALL c:\Progra~1\ArcGIS\Pro\bin\Python\scripts\propy.bat .\src\main\py\create_gdb.py %SQLCMDSERVER% %SQLCMDDBNAME% %AUTHFILE%
+CALL c:\Progra~1\ArcGIS\Pro\bin\Python\scripts\propy.bat .\src\main\py\create_gdb.py %SQLCMDSERVER% %DBNAME% %AUTHFILE%
 REM A TEST HERE WOULD BE GOOD
